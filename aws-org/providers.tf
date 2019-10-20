8e547960ffbfa7862fade33f77e5e4448a311be6
Sascha Eglau <se@saschaeglau.com>
2019-10-20 20:18:18 +0800

    use region from variables

--- aws-org/providers.tf
+++ aws-org/providers.tf
@@ -1,5 +1,9 @@
 provider "aws" {
   version = "~> 2.31"
-  region  = "us-east-1"
+  region  = var.region
   profile = var.profile
+}
+
+provider "null" {
+  version = "~> 2.1"
 }
--- aws-org/variables.tf
+++ aws-org/variables.tf
@@ -2,3 +2,4 @@ variable "bucket" {}
 variable "dynamodb_table" {}
 variable "keybase_username" {}
 variable "profile" {}
+variable "region" {}
