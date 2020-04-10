c462561018d41419e149a3858662a81afe96627e
Sascha Eglau <se@saschaeglau.com>
2020-04-10 12:46:07 +0800

    remove terraform iam user

--- terraform-modules/aws-account/main.tf
+++ terraform-modules/aws-account/main.tf
@@ -10,7 +10,7 @@ resource "aws_organizations_account" "this" {
 
   # There is no AWS Organizations API for reading role_name
   lifecycle {
-    ignore_changes = ["role_name"]
+    ignore_changes = [role_name]
   }
 }
 
@@ -18,24 +18,4 @@ resource "aws_iam_account_alias" "this" {
   provider = aws.new_account
 
   account_alias = var.name
-}
-
-resource "aws_iam_user" "terraform" {
-  provider = aws.new_account
-
-  name = "terraform"
-}
-
-resource "aws_iam_access_key" "terraform" {
-  provider = aws.new_account
-
-  user    = aws_iam_user.terraform.name
-  pgp_key = "keybase:${var.keybase_username}"
-}
-
-resource "aws_iam_user_policy_attachment" "terraform_admin" {
-  provider = aws.new_account
-
-  user       = aws_iam_user.terraform.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
--- terraform-modules/aws-account/outputs.tf
+++ terraform-modules/aws-account/outputs.tf
@@ -1,11 +1,3 @@
-output "access_key_id" {
-  value = aws_iam_access_key.terraform.id
-}
-
-output "access_key_secret" {
-  value = aws_iam_access_key.terraform.encrypted_secret
-}
-
 output "id" {
   value = aws_organizations_account.this.id
 }
