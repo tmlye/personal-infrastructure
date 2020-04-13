3dcf258bb589ca534c461d8aed2e15684212a958
Sascha Eglau <se@saschaeglau.com>
2020-04-13 10:59:05 +0800

    attach policy to the user, delete group

--- aws-org/iam.tf
+++ aws-org/iam.tf
@@ -2,19 +2,7 @@ resource "aws_iam_user" "terraform" {
   name = "terraform"
 }
 
-resource "aws_iam_group" "admins" {
-  name = "Admins"
-}
-
-resource "aws_iam_user_group_membership" "terraform_admins" {
-  user = aws_iam_user.terraform.name
-
-  groups = [
-    aws_iam_group.admins.name
-  ]
-}
-
-resource "aws_iam_group_policy_attachment" "admins" {
-  group      = aws_iam_group.admins.name
+resource "aws_iam_user_policy_attachment" "terraform" {
+  user       = aws_iam_user.terraform.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
