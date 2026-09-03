3bc498ed6ac27dee4164fc75fbbf3e48e71e6ab9
Sascha Eglau <se@saschaeglau.com>
2026-09-03 22:06:55 +0200

    Remove the AWS stacks for saschaeglau.com
    The site runs on Cloudflare Pages and the DNS zone is on Cloudflare.
    The saschaeglaucom and mail stacks are destroyed.

--- mail/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type        = string
-  description = "AWS region to use for the provider"
-}
-
-variable "profile" {
-  type        = string
-  description = "AWS profile to use for the provider"
-}
